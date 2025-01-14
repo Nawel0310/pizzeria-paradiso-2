91656a79d4be839f922855ed28f5a096b5ddedaf
Nawel0310 <nbarbaro@frba.utn.edu.ar>
2025-01-14 17:01:23 -0300

    mejorado sitemap

--- app/sitemaps.xml/route.js
+++ app/sitemaps.xml/route.js
@@ -5,10 +5,11 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export async function GET() {
   const routes = [
     { path: '', changefreq: 'monthly', priority: 1.0 },
-    { path: '#aboutUs', changefreq: 'monthly', priority: 0.8 },
-    { path: '#especialidades', changefreq: 'monthly', priority: 0.8 },
-    { path: '#testimonios', changefreq: 'monthly', priority: 0.8 },
-    { path: '#contacto', changefreq: 'monthly', priority: 0.8 },
+    { path: '#heroSection', changefreq: 'monthly', priority: 1.0 },
+    { path: '#aboutUsSection', changefreq: 'monthly', priority: 0.8 },
+    { path: '#especialidadesSection', changefreq: 'monthly', priority: 0.8 },
+    { path: '#testimoniosSection', changefreq: 'monthly', priority: 0.8 },
+    { path: '#contactoSection', changefreq: 'monthly', priority: 0.8 },
   ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
